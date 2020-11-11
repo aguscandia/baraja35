0f2c1c6b874f10260a57ca8d00202fb4252d1319
agustin <a.ti.21.ac@gmail.com>
2020-11-11 19:12:56 -0300

    tercer commit

--- baraja.py
+++ baraja.py
@@ -9,3 +9,25 @@ def crearBaraja():
             baraja.append(numero+palo)
     return baraja
 
+
+def intercambio(primer_valor, segundo_valor):
+    aux = primer_valor
+    primer_valor = segundo_valor
+    segundo_valor = aux
+    return primer_valor, segundo_valor
+
+
+
+def barajar(lista_de_naipes):                                                                                         
+    for i in range(len(lista_de_naipes)):
+        nueva_pos = random.randrange(len(lista_de_naipes))
+        
+        #intercambio de cartas, tecnica vaso vacio
+        
+        aux = lista_de_naipes[nueva_pos]
+        lista_de_naipes[nueva_pos] = lista_de_naipes[i]
+        lista_de_naipes[i] = aux
+    return lista_de_naipes
+
+print (crearBaraja())
+    
